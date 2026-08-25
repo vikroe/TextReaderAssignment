ffc924b803c1da2f5ba0ac21f7ac845310235429
Viktor-Adam Koropecký <v.koropecky@protonmail.com>
2026-08-25 22:30:29 +0200

    Add a dummy text source

---
+++ TextReader.Core/Sources/ILineSource.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextReader.Core.Sources
+{
+    internal interface ILineSource
+    {
+        long LineCount { get; }
+        string GetLine(long index);
+    }
+}
---
+++ TextReader.Core/Sources/SyntheticSource.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextReader.Core.Sources
+{
+    internal class SyntheticSource: ILineSource
+    {
+        public long LineCount => 500_000_000;
+        public string GetLine(long index)
+        {
+            return $"Line {index}: " + new string('x', (int)(index % 120));
+        }
+    }
+}
